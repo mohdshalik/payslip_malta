3840172fb93c442667a73cd940fac343a38a9b34
mohdshalik <shaalmohd@gmail.com>
2025-08-21 00:12:10 +0530

    cors

--- payroll/payroll/settings.py
+++ payroll/payroll/settings.py
@@ -54,11 +54,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://167.86.74.89",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://167.86.74.89",
+# ]
 
 ROOT_URLCONF = 'payroll.urls'
 
