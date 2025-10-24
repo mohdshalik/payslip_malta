72f2a286d38e3fbdf34e0c895ec71f1110a78ffe
mohdshalik <shaalmohd@gmail.com>
2025-10-24 14:58:30 +0530

    new updates

--- payroll/payroll/settings.py
+++ payroll/payroll/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = 'django-insecure-dc4(!c1n1lz^d8_=#vt^5+)6!#*o)-9_kx#_#85)**!lotf$l6
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-    
-ALLOWED_HOSTS = ['84.247.143.75','127.0.0.1','localhost']
 
+ALLOWED_HOSTS = ['84.247.143.75','127.0.0.1','localhost']
+CSRF_TRUSTED_ORIGINS = ['http://84.247.143.75']
 
 # Application definition
 
